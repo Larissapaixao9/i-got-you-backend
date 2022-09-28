fe487d722d81e59a45fa22a1df2e9eb33771f2a3
Larissa Paixao <laryssapaixao9@gmail.com>
2022-09-28 17:27:54 -0300

    first commit

---
+++ src/server.ts
@@ -0,0 +1,18 @@
+import express from 'express'
+import cors from 'cors'
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+const app = express()
+
+//middlewares
+app.use(cors())
+app.use(express.json())
+
+const PORT = process.env.PORT
+
+
+
+
+app.listen(PORT, ()=>console.log(`listening on port ${PORT}`))
