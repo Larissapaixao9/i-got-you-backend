ce17cf197f079694e895c2e6602d25ec34c8a43a
Larissa Paixao <laryssapaixao9@gmail.com>
2022-10-09 14:56:00 -0300

    add sign-in integration test

--- tests/integration/app.test.ts
+++ tests/integration/app.test.ts
@@ -16,7 +16,7 @@ beforeEach(async () => {
     await prisma.$disconnect();
   });
 
-  describe('Testa POST /signup ', () => {
+  describe('Testa POST /sign-up ', () => {
     it('Deve retornar 201, se cadastrado usuario no formato correto', async()=>{
       const body= await auth_factory.fixed_user()
   
@@ -51,3 +51,66 @@ beforeEach(async () => {
       expect(status).toEqual(409)
     });
   });
+
+  describe('Testa POST /sign-in', ()=>{
+    it('Deve retornar 200 se o login for bem sucedido', async()=>{
+      
+      const user = await auth_factory.fixed_user()
+
+      const body = await auth_factory.user_login()
+
+      await supertest(app).post('/sign-up').send(user)
+
+      const result = await supertest(app).post('/sign-in').send(body)
+
+      const status = result.status
+
+      expect(status).toEqual(200)
+
+    })
+
+    it('Deve retornar 401 se a senha digitada estiver incorreta',async()=>{
+
+      const user = await auth_factory.fixed_user()
+
+      const body = await auth_factory.user_login_wrong_password()
+
+      await supertest(app).post('/sign-up').send(user)
+
+      const result = await supertest(app).post('/sign-in').send(body)
+
+      const status = result.status
+
+      expect(status).toEqual(401)
+    })
+
+    it('Deve retornar 404 se email digitado estiver incorreto',async()=>{
+
+      const user = await auth_factory.fixed_user()
+
+      const body = await auth_factory.user_login_wrong_email()
+
+      await supertest(app).post('/sign-up').send(user)
+
+      const result = await supertest(app).post('/sign-in').send(body)
+
+      const status = result.status
+
+      expect(status).toEqual(404)
+    })
+
+    it('Deve retornar 422 se vier informações vazias',async()=>{
+
+      const user = await auth_factory.fixed_user()
+
+      const body = await auth_factory.user_login_empty()
+
+      await supertest(app).post('/sign-up').send(user)
+
+      const result = await supertest(app).post('/sign-in').send(body)
+
+      const status = result.status
+
+      expect(status).toEqual(422)
+    })
+  })
