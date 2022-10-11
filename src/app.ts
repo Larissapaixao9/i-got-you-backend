2a473f8b5a193129daeb6a3113678dc068e6daa7
Larissa Paixao <laryssapaixao9@gmail.com>
2022-10-11 20:26:34 -0300

    change app file to correct CORS error

--- src/app.ts
+++ src/app.ts
@@ -5,9 +5,23 @@ import  {errorHanddlingMiddleware}  from '../src/middlewares/error_handler_middl
 import router from '../src/routers/mainRouter'
 import dotenv from 'dotenv'
 dotenv.config()
+const app = express();
 const domainsFromEnv = process.env.CORS_DOMAINS || ""
 
-const app = express();
+const whitelist = domainsFromEnv.split(",").map(item => item.trim())
+
+const corsOptions = {
+  origin: function (origin:any, callback:any) {
+    if (!origin || whitelist.indexOf(origin) !== -1) {
+      callback(null, true)
+    } else {
+      callback(new Error("Not allowed by CORS"))
+    }
+  },
+  credentials: true,
+}
+
+app.use(cors(corsOptions))
 app.use(cors());
 app.use(express.json());
 app.use(router)
